7feaf89261e6e1d21ab4b2bbdd1d04d5646f598c
Vladyslav <vkadyrov@protonmail.com>
2023-01-04 18:47:16 -0500

    Nav component was revised

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,10 @@
 import React from 'react';
 
-export default function Nav() {}
+export default function Nav() {
+  return (
+    <div>
+      <h2>My Movie List</h2>
+      <img alt='movie-list-logo'></img>
+    </div>
+  );
+}
