7235e3e7679e4de148189a9adc7a79c77a832faf
Vladyslav <vkadyrov@protonmail.com>
2023-01-05 11:59:27 -0500

    Basic Route was added to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
+import MovieList from './MovieList';
+import About from './About';
 
 export default function App() {
   return (
     <div>
       <Nav />
+      <Switch>
+        <Route exact path='/movielist'>
+          <MovieList />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+      </Switch>
     </div>
   );
 }
