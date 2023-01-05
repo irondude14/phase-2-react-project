f3a6053bdb7b40d2ad6fd622c9f1cb387d884195
Vladyslav <vkadyrov@protonmail.com>
2023-01-05 12:34:47 -0500

    Updates to the names of the components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Nav from './Nav';
-import MoviesList from './MovieList';
+import MoviesList from './MoviesList';
 import About from './About';
 
 export default function App() {
