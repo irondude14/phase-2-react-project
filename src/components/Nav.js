269f68c9a9f231927237561b393f13f4af07623c
Vladyslav <vkadyrov@protonmail.com>
2023-01-05 11:56:01 -0500

    Added NavLink & Navigation to Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,6 +6,12 @@ export default function Nav() {
     <div>
       <h2>My Movie List</h2>
       <img alt='movie-list-logo'></img>
+      <NavLink to='/movielist' exact>
+        MovieList
+      </NavLink>
+      <NavLink to='/about' exact>
+        About
+      </NavLink>
     </div>
   );
 }
